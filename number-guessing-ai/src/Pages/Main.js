1cf53f16125dad11b7fac86d20ba6d3a15822153
MSGkingdom <157904656+MSGkingdom@users.noreply.github.com>
2024-07-27 21:57:57 +0300

    Added react

---
+++ number-guessing-ai/src/Pages/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+function Main() {
+  return (
+    <div>
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Main
