64ec146804ccee5d5422c72ef3f8f2178d15b5bc
MSGkingdom <157904656+MSGkingdom@users.noreply.github.com>
2024-07-30 13:41:38 +0300

    Update Network.js

--- number-gusessing-backend/Network.js
+++ number-gusessing-backend/Network.js
@@ -1,9 +1,35 @@
 class Network{
-    constructor(){
+    constructor(inputSize,outputSize,innerLayersSize=16,innerLayersCount=6){
+        if(innerLayersCount<1) throw new Error("The parameter innerLayersCount must be at least 1") ;
         this.weights = [];
         this.neurons =[];
         this.biases = [];
-        initNetWork();
-
+        initNetwork(inputSize,outputSize,innerLayersSize);
     }
+    initNetwork(inputSize,outputSize,innerLayersSize){
+        let layerWeights = [];
+        for(let i =0;i<inputSize;i++){
+            let neuronWeights =[];
+            for(let i =0;i<innerLayersSize;i++){
+                neuronWeights.push(Math.random());
+            }
+            layerWeights.push(neuronWeights);
+        }
+        weights.push(layerWeights);
+        for(let i =0;i<innerLayersCount-1;i++){
+            layerWeights = []
+            for(let j =0;j<innerLayersSize;j++){
+                let neuronWeights =[];
+                for(let i =0;i<innerLayersSize;i++){
+                    neuronWeights.push(Math.random());
+                }
+                layerWeights.push(neuronWeights);
+            }
+            weights.push(layerWeights);
+        }   
+        for(let i =0;i<inputSize;i++){
+            
+        }
+    }
+    
 }
